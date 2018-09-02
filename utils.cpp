d4493c9209bcda7469f1c4e5f1059a5bc4076b5c
Florian Leeber <flori@bin.org.in>
2018-09-02 18:39:10 +0200

    Cleanuup in utils

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,4 @@
 #include "utils.h"
-#include <QDebug>
 
 qint64 QmlUtils::getUnifiedMessageKey(qint32 messageId, qint32 peerId)
 {
@@ -8,7 +7,6 @@ qint64 QmlUtils::getUnifiedMessageKey(qint32 messageId, qint32 peerId)
         result = 0;
     else
         result = (static_cast<qint64>(peerId) << 32) + messageId;
-    //qWarning() << "getUnifiedMessageKey: mId=" << messageId << ", peer=" << peerId << ", result=" << result;
     return result;
 }
 
@@ -17,7 +15,6 @@ qint32 QmlUtils::getSeparatePeerId(qint64 unifiedKey)
     qint64 result;
 
     result = static_cast<qint32>(unifiedKey >> 32);
-    //qWarning() << "getSeparatePeerId: unifiedId=" << unifiedKey << ", peer=" << result;
     return result;
 }
 
@@ -26,6 +23,7 @@ qint32 QmlUtils::getSeparateMessageId(qint64 unifiedKey)
     qint64 result;
 
     result = static_cast<qint32>((unifiedKey << 32) >> 32);
-    //qWarning() << "getSeparateMessageId: unifiedId=" << unifiedKey << ", msgid=" << result;
     return result;
 }
+
+
--- utils.h
+++ utils.h
@@ -9,10 +9,12 @@
 
 class QmlUtils
 {
+
 public:
     static qint64 getUnifiedMessageKey(qint32 messageId, qint32 peerId);
     static qint32 getSeparateMessageId(qint64 unifiedKey);
     static qint32 getSeparatePeerId(qint64 unifiedKey);
+
 };
 
 #endif // QMLUTILS_H
